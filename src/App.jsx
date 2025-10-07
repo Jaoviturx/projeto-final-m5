6134b8f9592acf6f2a803cd92ebf4cfbcb527aa1
TabsMacedo <tabata.macedo1897@gmail.com>
2025-10-07 16:50:32 -0300

    adição mudança Tábata

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
       <div>
@@ -18,9 +17,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+       <p>Tabs passou aqui</p>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
