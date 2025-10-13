750d6a9183f688cc3306cf30d82c0ba0aa52149b
TabsMacedo <tabata.macedo1897@gmail.com>
2025-10-13 13:37:27 -0300

    feat: correções no card de Clima

--- src/components/Clima/Clima.jsx
+++ src/components/Clima/Clima.jsx
@@ -3,33 +3,39 @@ import styles from "./Clima.module.css";
 
 function Clima() {
   const [climates, setClimates] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch("https://projeto-final-m4-u5yq.onrender.com/climate")
       .then((response) => response.json())
       .then((data) => setClimates(data))
-      .catch((error) => console.error("Erro ao buscar dados:", error));
+      .catch((error) => console.error("Erro ao buscar dados:", error))
+      .finally(() => setLoading(false));
   }, []);
 
   return (
     <main className={styles.main}>
-      <h1>Dados sobre o clima Mundial</h1>
+      <h1>Dados sobre o Clima Mundial</h1>
       <div className={styles.container}>
-      {climates.length > 0 ? (
-        climates.map((item) => (
-          <div key={item.id} className={styles.card}>
-            <h2 className={styles.title}>{item.nome}</h2>
-            <p className={styles.description}>{item.descricao}</p>
-            <p className={styles.year}>Ano: {item.ano}</p>
-            <p className={styles.impact}>Impacto: {item.impacto}</p>
+        {loading ? (
+          <div className={styles.loaderContainer}>
+            <div className={styles.loader}></div>
+            <p>Carregando dados climáticos...</p>
           </div>
-        ))
-      ) : (
-        <p className={styles.loading}>Carregando dados climáticos...</p>
-      )}
-    </div>
+        ) : climates.length > 0 ? (
+          climates.map((item) => (
+            <div key={item.id} className={styles.card}>
+              <h2 className={styles.title}>{item.nome}</h2>
+              <p className={styles.description}>{item.descricao}</p>
+              <p className={styles.year}>Ano: {item.ano}</p>
+              <p className={styles.impact}>Impacto: {item.impacto}</p>
+            </div>
+          ))
+        ) : (
+          <p>Nenhum dado encontrado.</p>
+        )}
+      </div>
     </main>
-    
   );
 }
 
