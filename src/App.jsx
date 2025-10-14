e38ccd1192db5fcfe38bc6ac0ddfccc561273f94
Jaoviturx <joaovitorcruci555@gmail.com>
2025-10-14 20:59:20 -0300

    importando

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Clima from "./components/Clima/Clima.jsx";
 import Menu from "./components/Menu/Menu.jsx";
 import Desmatamento from "./components/Desmatamento/Desmatamento.jsx";
-
+import Banner from "./components/Banner/Banner.jsx";
 function App() {
   return (
     <div>
       <Menu />
+      <Banner />
       <Clima />
       <Desmatamento />
     </div>
