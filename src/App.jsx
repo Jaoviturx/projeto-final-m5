bdad597219c290cc20fbcb676054ddb2ae448525
vlaraa11 <vl7829765@gmail.com>
2025-10-14 22:18:36 -0300

    parte footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import Clima from "./components/Clima/Clima.jsx";
 import Menu from "./components/Menu/Menu.jsx";
 import Desmatamento from "./components/Desmatamento/Desmatamento.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 import Banner from "./components/Banner/Banner.jsx";
+
 function App() {
   return (
     <div>
@@ -9,6 +11,7 @@ function App() {
       <Banner />
       <Clima />
       <Desmatamento />
+      <Footer />
     </div>
   );
 }
